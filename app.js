ba5513751dc87155dc8933f35b3192ce68f06ed7
glendex <glendasp@gmail.com>
2016-03-23 16:07:26 -0500

    Added stylesheet to allflowers and flowersDetails

--- app.js
+++ app.js
@@ -1,75 +1,87 @@
-var express = require("express");
-var MongoClient = require("mongodb").MongoClient;
-var engines = require("jade");
+
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var engines = require('jade');
 var assert= require('assert');
 var bodyParser = require('body-parser');
 
 app = express();
 
-app.set("view engine", "jade");
-app.set("views", __dirname + "/views");
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
 
-app.use(express.static("public"));
+//tell express where our static  files are
+app.use(express.static('public'));
 
-//bodyParser needed to enables routes to extract request body.
+//bodyParser needed to enable routes to extract request body, used in POST requests
 app.use(bodyParser.urlencoded({ extended : true }));
 
-
-
 //Attempt to connect to MongoDB.
-MongoClient.connect("mongodb://localhost:27017/garden", function(err, db){
+MongoClient.connect('mongodb://localhost:27017/garden', function(err, db){
+
   assert.equal(null, err);  //This will crash the app if there is an error.
-  console.log("Connected to MongoDB");
+
+  console.log('Connected to MongoDB');
 
   //routes - this is for the home page
   app.get('/', function(req, res){
-    db.collection('flowers').find({}, {"name": true, "color": true}).toArray(function(err, flowerdocs){
+
+    // Make query for unique colors, to populate the dropdown box
+    db.collection('flowers').distinct('color', function(err, colorDocs) {
       if (err) { return res.sendStatus(500); }
 
-      var colordocs = db.collection('flowers').distinct("color", function(err, colordocs){
-        if (err) { return res.sendStatus(500); }
-        return res.render('allflowers', {'flowers' : flowerdocs, "flowerColors":colordocs});
-      })
+      //If selecting a color and clicked the colorButton, this has a value
+      if (req.query.colorButton) {
+        var color = req.query.colorDropDown;    //What color was selected?
+        //Get all of the flowers of the desired color.
+        db.collection('flowers').find({'color': color}).toArray(function(err, flowerDocs) {
+          if (err) { return res.sendStatus(500); }
+          //Optional - turn 'red' into 'Red' for display
+          var displayColor = color.slice(0,1).toUpperCase() + color.slice(1, color.length)
+          //return res.render statement inside a callback to prevent further processing of response
+
+          console.log(err);
+          //console.log('count?' + count);
+
+          return res.render('allflowers',
+              { 'flowers' : flowerDocs, 'currentColor' : displayColor, 'flowerColors' : colorDocs } );
+        });
+      }
+
+      //If visiting the home page, or clicking the clear button, then fetch all of the flowers
+      else {
+        db.collection('flowers').find().toArray(function(err, flowerDocs){
+          if (err) { return res.sendStatus(500); }
+          return res.render('allFlowers', { 'flowers' : flowerDocs, 'flowerColors' : colorDocs } );
+        });
+      }
     });
   });
 
-  //Form-handling route - show only flowers of selected color
-  app.get("/showColors", function(req, res){
-    var color = req.query.colorDropDown;
-    //Get all of the flowers of the desired color. Only return name and color.
-    db.collection('flowers').find({"color": color}, {"name": true, "color": true}).toArray(function(err, docs){
 
-      if (err) { return rres.sendStatus(500); }
+  //show info of .....
+  app.get('/details/:flower', function(req, res) {
+    var flowerName = req.params.flower;
+    //Find document with this flower's name. Only expecting one result so can
+    //use limit(1) function to minimise work for DB
+    db.collection('flowers').find( {'name' : flowerName } ).limit(1).toArray(function(err, docs) {
+      if (err) { console.log(err); return res.sendStatus(500); }
+      //If other than 1 result, no results. Send 404 not found
+      if (docs.length != 1) { console.log(docs);  return res.sendStatus(404); }
 
-      var colordocs = db.collection('flowers').distinct("color", function(err, colordocs){
-        if (err) { return res.sendStatus(500); }
-        //Turn "red" into "Red" for display
-        var displayColor = color.slice(0,1).toUpperCase() + color.slice(1, color.length)
-        //return res.render statement recommended inside a callback to prevent further processing of res
-        return res.render('allflowers',
-           { 'flowers' : docs, "currentColor" : displayColor, "flowerColors" : colordocs });
-    });
+      return res.render('flowerDetails', docs[0]);
     });
   });
 
-  app.get("/details/:flower", function(req, res){
-
-    var flowerName = req.params.flower //Get value of "flower" param
-    //DB query for this flower. Use findOne and note the callback.
-    db.collection("flowers").findOne({"name": flowerName}, function(err, doc){
-      if (err) { return res.sendStatus(500); }
-      console.log(doc);
-      return res.render("flowerDetails", doc)
-    })
-  });
 
 
   app.post("/addNewFlower", function(req, res) {
-     db.collection("flowers").insert(req.body, function(err, result){
-       if (err) { return res.sendStatus(500); }
-       return res.redirect('/'); //todo send success/fail back to client.
-     });
+    db.collection("flowers").insert(req.body, function(err, result){
+      if (err) { return res.sendStatus(500); }
+      return res.redirect('/'); //todo send success/fail back to client.
+    });
   });
+
 
 
   app.put("/updateColor", function(req, res) {
@@ -92,16 +104,15 @@ MongoClient.connect("mongodb://localhost:27017/garden", function(err, db){
   });
 
 
-
   //All other requests, return 404 not found
   app.use(function(req, res){
     res.sendStatus(404);
   });
 
+
   //And start the server
   var server = app.listen(3050, function(){
     var port = server.address().port;
-    console.log("Server listening on port "+ port);
+    console.log('Server listening on port '+ port);
   });
-
-});
+});  //End of MongoClient callback
